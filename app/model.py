5a25390370e411e9698f5873d483ccafffd9c03e
code9633 <shashikaudara5566@gmail.com>
2023-10-12 12:01:10 +0530

    changes

--- app/model.py
+++ app/model.py
@@ -1,2 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Song(models.Model):
+    
+    labels = models.IntegerField(('labels'))
+    uri = models.CharField(("uri"), max_length= 255 )
--- app/views.py
+++ app/views.py
@@ -8,72 +8,18 @@ import numpy as np
 import tensorflow as tf
 
 from app.forms import UserForm
+from .model import Song
 
-model = tf.keras.models.load_model('./files/model/final_model_V3.h5')
+model = tf.keras.models.load_model('./models/final_model_V3.h5')
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the login Page")
+    
+    emotion = request.session.get('e')
+    return render(request, "homepage/homepage.html")
 
-# def predict_emotion(request):
-    
-#     print(request.POST)
-    
-#     if request.method == 'POST':
-        
-#         # get the image data  from the POST request as a Base64-encoded string
-#         image_data = request.POST.get('image', None)
-        
-#         if image_data :
-#             #decode the Base64  image data to byte
-#             image_byte = base64.b64decode(image_data)
-            
-#             #create a Numpy array from the bytes
-#             image_np = np.frombuffer(image_byte, np.uint8)
-            
-#             image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
-#             # convert the image in to gray image
-#             faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 
-#                                                 'files/cascade_file/haarcascade_frontalface_default.xml')
-#             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            
-#             faces = faceCascade.detectMultiScale(gray, 1.1, 4)
-#             for x,y,w,h in faces:
-#                 roi_gray = gray[y:y+h, x:x+w]
-#                 roi_color = image[y:y+h, x:x+w]
-#                 cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0,0), 2) #BGR
-#                 faces = faceCascade.detectMultiScale(roi_gray)
-#                 if len(faces) == 0:
-#                     print("Face not detected")
-#                 else:
-#                     for (ex,ey, ew, eh) in faces:
-#                         face_roi = roi_color[ey: ey+eh, ex: ex+ew]
-        
-    
-#             final_image = cv2.resize(face_roi, (48,48))
-#             final_image = np.expand_dims(final_image, axis=0)
-#             final_image = final_image/255.0
-            
-#             #load the model
-            
-#             
-            
-#             predict_emotion = model.predict(final_image)
-#             int_prediction = np.argmax(predict_emotion)
-            
-#             labels = {'0' : 'Angry', '1':'Happy', '2': 'sad', '3' : 'Neutral'}
-    
-#             prediction = labels[int_prediction]
-            
-#             return redirect('result', prediction = prediction)
-        
-#         else :
-#             return JsonResponse({'error' : 'Invalid image data'})
-#     else:
-#         return JsonResponse({'error': 'Invalid request'})  
-    
 def predict_emotion(request):
     
     if request.method == 'POST':
@@ -87,48 +33,49 @@ def predict_emotion(request):
 
             # Decode the image using OpenCV
             image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
             # convert the image in to gray image
-            
-            print(image.shape)
-            
-            faceCascade =  cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             
             faces = faceCascade.detectMultiScale(gray, 1.1, 4)
-            for x,y,w,h in faces:
+            
+            # Initialize face_roi outside the loop
+            face_roi = None
+            
+            for x, y, w, h in faces:
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = image[y:y+h, x:x+w]
-                cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0,0), 2) #BGR
+                cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)  # BGR
                 faces = faceCascade.detectMultiScale(roi_gray)
                 if len(faces) == 0:
                     print("Face not detected")
                 else:
-                    for (ex,ey, ew, eh) in faces:
-                        face_roi = roi_color[ey: ey+eh, ex: ex+ew]
-        
-    
-            final_image = cv2.resize(face_roi, (48,48))
-            final_image = np.expand_dims(final_image, axis=0)
-            final_image = final_image/255.0
-               
+                    for (ex, ey, ew, eh) in faces:
+                        face_roi = roi_color[ey:ey+eh, ex:ex+ew]
             
-            predict_emotion = model.predict(final_image)
-            int_prediction = np.argmax(predict_emotion)
-            
-            print(int_prediction)
-            
-            labels = {0 : 'Angry', 1 :'Happy', 2 : 'sad', 3 : 'Neutral'}
-    
-            emotion = labels[int_prediction]
-            
+            if face_roi is not None:
+                final_image = cv2.resize(face_roi, (48, 48))
+                final_image = np.expand_dims(final_image, axis=0)
+                final_image = final_image / 255.0
+
+                predict_emotion = model.predict(final_image)
+                int_prediction = np.argmax(predict_emotion)
+
+                # labels = {0: 'Angry', 1: 'Happy', 2: 'Sad', 3: 'Neutral'}
+
+                # emotion = labels[int_prediction]
+                
+                request.session['emotion'] = int_prediction
+                
+                return render(request, 'homepage/homepage.html')
+            else:
+                return JsonResponse({'error': 'Face not detected'})
+
+    return render(request, 'predict_emotion.html')
 
 
-            return render(request, 'app/result.html', {'emotion': emotion})
-
-    return render(request, 'app/predict_emotion.html')
-
-
-
+########### User Authentication #########################3
 
 def signupPage(request):
     form = UserForm()
@@ -140,7 +87,7 @@ def signupPage(request):
             messages.success(request, 'Login to Continue')
             return redirect('/login')
     
-    return render(request, 'app/signupPage.html', {'form' : form})
+    return render(request, 'authentication/signupPage.html', {'form' : form})
 
 def loginPage(request):
     
@@ -158,7 +105,7 @@ def loginPage(request):
             messages.error(request, "Invalid Username and Passord")
             return redirect('/login')
         
-    return render(request, 'app/loginPage.html')
+    return render(request, 'authentication/loginPage.html')
         
     
         
